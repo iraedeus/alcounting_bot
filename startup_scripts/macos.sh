d1c713b387bea2f60009e8e1f400e506ea2d2011
Morozov Nikita <nikitwww@gmail.com>
2024-03-12 15:46:01 +0300

    BOT_TOKEN relocated from file to environment variables, update README, startup scripts and main.yml

--- startup_scripts/macos.sh
+++ startup_scripts/macos.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if  [ "" == "${BOT_TOKEN}" ];
+then 
+  echo "variable BOT_TOKEN doesn't exist, see README.md, to get more info."
+  echo "shutting down"
+  exit
+fi
+
 SCRIPT_PATH="${BASH_SOURCE}"
 while [ -L "${SCRIPT_PATH}" ]; do
   SCRIPT_DIR="$(cd -P "$(dirname "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
