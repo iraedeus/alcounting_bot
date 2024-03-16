29b886be891d82081cb08520820b4041db54b55a
Morozov Nikita <nikitwww@gmail.com>
2024-03-16 06:07:35 +0300

    feat: add a template menu for customer

--- src/modules/customer.py
+++ src/modules/customer.py
@@ -1,3 +1,67 @@
 # Покупатель (Заказывающий) может заказать коктейль из списка.
 # При заказе должно быть оконо подтверждения или отмены заказа, после этого заказ попадает в очередь к бармену.
 # Здеся либо класс, либо просто написать логику для менюшек, какие-то функции только для покупателя и прочее, прочее.
+import logging
+
+from telegram import ForceReply, Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.constants import ParseMode
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, CallbackContext
+
+# Pre-assign menu text
+FIRST_MENU = "<b>Menu 1</b>\n\nA beautiful menu with a shiny inline button."
+SECOND_MENU = "<b>Menu 2</b>\n\nA better menu with even more shiny inline buttons."
+
+# Pre-assign button text
+NEXT_BUTTON = "Next"
+BACK_BUTTON = "Back"
+TUTORIAL_BUTTON = "Tutorial"
+
+# Build keyboards
+FIRST_MENU_MARKUP = InlineKeyboardMarkup([[
+    InlineKeyboardButton(NEXT_BUTTON, callback_data=NEXT_BUTTON)
+]])
+SECOND_MENU_MARKUP = InlineKeyboardMarkup([
+    [InlineKeyboardButton(BACK_BUTTON, callback_data=BACK_BUTTON)],
+    [InlineKeyboardButton(TUTORIAL_BUTTON, url="https://core.telegram.org/bots/api")]
+])
+
+
+async def menu(update: Update, context: CallbackContext) -> None:
+    """
+    This handler sends a menu with the inline buttons we pre-assigned above
+    """
+    logging.getLogger(__name__).info(f'{update.message.from_user.id} use {update.message.text}')
+
+    await context.bot.send_message(
+        update.message.from_user.id,
+        FIRST_MENU,
+        parse_mode=ParseMode.HTML,
+        reply_markup=FIRST_MENU_MARKUP
+    )
+
+
+async def button_tap(update: Update, context: CallbackContext) -> None:
+    """
+    This handler processes the inline buttons on the menu
+    """
+
+    data = update.callback_query.data
+    text = ''
+    markup = None
+
+    if data == NEXT_BUTTON:
+        text = SECOND_MENU
+        markup = SECOND_MENU_MARKUP
+    elif data == BACK_BUTTON:
+        text = FIRST_MENU
+        markup = FIRST_MENU_MARKUP
+
+    # Close the query to end the client-side loading animation
+    await update.callback_query.answer()
+
+    # Update message content with corresponding menu section
+    await update.callback_query.edit_message_text(
+        text,
+        ParseMode.HTML,
+        reply_markup=markup
+    )
